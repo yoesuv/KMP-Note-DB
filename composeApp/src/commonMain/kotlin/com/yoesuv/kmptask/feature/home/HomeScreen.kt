a5c849071b18156b5b4acde43fca729567d75beb
Yusuf Saifudin <yusuf.saifudin@gmail.com>
2025-09-25 06:12:53 +0700

    Adds floating action button to home screen
    Adds a floating action button with an add icon to the home screen.
    This button will be used to add a new task.

--- composeApp/src/commonMain/kotlin/com/yoesuv/kmptask/feature/home/HomeScreen.kt
+++ composeApp/src/commonMain/kotlin/com/yoesuv/kmptask/feature/home/HomeScreen.kt
@@ -5,13 +5,18 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
 import androidx.compose.material.icons.filled.Delete
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import com.yoesuv.kmptask.core.theme.AppColors
 import com.yoesuv.kmptask.feature.components.AppTopBar
 import kmpmytask.composeapp.generated.resources.Res
 import kmpmytask.composeapp.generated.resources.app_name
@@ -28,6 +33,13 @@ fun HomeScreen() {
                 trailingIcon = Icons.Default.Delete,
                 onTrailingIconClick = { /* TODO: delete all function */ }
             )
+        },
+        floatingActionButton = {
+            FloatingActionButton(
+                containerColor = AppColors.Pink500,
+                onClick = { /* TODO: add new task */ }) {
+                Icon(Icons.Filled.Add, contentDescription = "Add task", tint = Color.White)
+            }
         }
     ) { padding ->
         Column(
