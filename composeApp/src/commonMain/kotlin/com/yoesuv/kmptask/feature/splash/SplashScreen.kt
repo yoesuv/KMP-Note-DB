c6447a99114fd487e7c2e3176f4ce738372701fc
Yusuf Saifudin <yusuf.saifudin@gmail.com>
2025-09-24 19:57:23 +0700

    Adds splash screen
    Implements splash screen with app name displayed.

--- composeApp/src/commonMain/kotlin/com/yoesuv/kmptask/feature/splash/SplashScreen.kt
+++ composeApp/src/commonMain/kotlin/com/yoesuv/kmptask/feature/splash/SplashScreen.kt
@@ -1,8 +1,29 @@
 package com.yoesuv.kmptask.feature.splash
 
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.sp
+import kmpmytask.composeapp.generated.resources.Res
+import kmpmytask.composeapp.generated.resources.app_name
+import org.jetbrains.compose.resources.stringResource
 
 @Composable
 fun SplashScreen() {
-
+    Box(
+        modifier = Modifier.fillMaxSize(),
+        contentAlignment = Alignment.Center
+    ) {
+        Text(
+            text = stringResource(Res.string.app_name),
+            fontSize = 18.sp,
+            fontWeight = FontWeight.Bold,
+            textAlign = TextAlign.Center
+        )
+    }
 }
