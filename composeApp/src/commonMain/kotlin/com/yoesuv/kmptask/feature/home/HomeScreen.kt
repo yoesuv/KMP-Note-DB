729873b1a2c3fa0983a1bbf9ff61eba251c4eefd
Yusuf Saifudin <yusuf.saifudin@gmail.com>
2025-10-09 05:57:14 +0700

    Adds empty task state to the home screen
    Implements a state for when there are no tasks.
    This displays a message informing the user that the task list is empty.

--- composeApp/src/commonMain/kotlin/com/yoesuv/kmptask/feature/home/HomeScreen.kt
+++ composeApp/src/commonMain/kotlin/com/yoesuv/kmptask/feature/home/HomeScreen.kt
@@ -1,5 +1,6 @@
 package com.yoesuv.kmptask.feature.home
 
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
@@ -11,12 +12,14 @@ import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import com.yoesuv.kmptask.core.db.rememberAppDatabase
@@ -25,6 +28,7 @@ import com.yoesuv.kmptask.core.theme.AppColors
 import com.yoesuv.kmptask.feature.components.AppTopBar
 import kmpmytask.composeapp.generated.resources.Res
 import kmpmytask.composeapp.generated.resources.app_name
+import kmpmytask.composeapp.generated.resources.empty_task
 import org.jetbrains.compose.resources.stringResource
 
 @Composable
@@ -37,6 +41,7 @@ fun HomeScreen() {
     val dao = remember(db) { db.myTaskDao() }
     val viewModel = remember(dao) { HomeViewModel(dao) }
     val tasks by viewModel.tasks.collectAsState()
+
     Scaffold(
         topBar = {
             AppTopBar(
@@ -49,19 +54,37 @@ fun HomeScreen() {
         floatingActionButton = {
             FloatingActionButton(
                 containerColor = AppColors.Pink500,
-                onClick = { showDialog = true }) {
+                onClick = { showDialog = true }
+            ) {
                 Icon(Icons.Filled.Add, contentDescription = "Add task", tint = Color.White)
             }
         }
     ) { padding ->
-        LazyColumn(
-            modifier = Modifier
-                .fillMaxSize()
-                .padding(padding)
-        ) {
-            items(tasks) { task ->
-                ItemTask(myTask = task, onMoreClick = { showTaskOptionsDialog = true; selectedTask = task })
-                HorizontalDivider()
+        if (tasks.isEmpty()) {
+            Box(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .padding(padding),
+                contentAlignment = Alignment.Center
+            ) {
+                Text(
+                    text = stringResource(Res.string.empty_task),
+                    color = Color.Gray
+                )
+            }
+        } else {
+            LazyColumn(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .padding(padding)
+            ) {
+                items(tasks) { task ->
+                    ItemTask(
+                        myTask = task,
+                        onMoreClick = { showTaskOptionsDialog = true; selectedTask = task }
+                    )
+                    HorizontalDivider()
+                }
             }
         }
     }
