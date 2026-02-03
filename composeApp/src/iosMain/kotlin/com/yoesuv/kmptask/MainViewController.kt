f645cb815ace8b7be11ee49d0db606823c723faf
Yusuf Saifudin <yusuf.saifudin@gmail.com>
2026-02-03 15:33:58 +0700

    feat: Integrates Koin DI framework
    Adds Koin dependency injection framework to the project.
    
    This allows for a more modular and testable architecture by
    centralizing dependency management.
    
    Removes direct database access from composable and use viewmodel

--- composeApp/src/iosMain/kotlin/com/yoesuv/kmptask/MainViewController.kt
+++ composeApp/src/iosMain/kotlin/com/yoesuv/kmptask/MainViewController.kt
@@ -1,5 +1,12 @@
 package com.yoesuv.kmptask
 
 import androidx.compose.ui.window.ComposeUIViewController
+import com.yoesuv.kmptask.di.appModule
+import org.koin.core.context.startKoin
 
-fun MainViewController() = ComposeUIViewController { App() }
+fun MainViewController() = ComposeUIViewController { 
+    startKoin {
+        modules(appModule)
+    }
+    App() 
+}
